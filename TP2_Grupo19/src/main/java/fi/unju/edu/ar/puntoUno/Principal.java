bace8ab13899c76a90f026bd4fcada29a68d8ab7
ignam03 <ignafree97@gmail.com>
2022-04-12 22:53:54 -0300

    punto1 realizado

---
+++ TP2_Grupo19/src/main/java/fi/unju/edu/ar/puntoUno/Principal.java
@@ -0,0 +1,28 @@
+package fi.unju.edu.ar.puntoUno;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Ingrese numero para el factorial");
+		int numero ;
+		int factorial = 1;
+		numero = teclado.nextInt();
+		
+		
+		
+		if(numero < 10) {
+			for(int i = 1; i <= numero; i++) {
+				factorial *= i;
+				
+			}
+			System.out.println("El factorial del numero " + numero + " es " + factorial);
+		}
+	}
+
+}
