15f5c2b7aebe893d7daca5807c65efd4356ef179
ignam03 <ignafree97@gmail.com>
2022-04-12 22:43:49 -0300

    tp2-grupo19

---
+++ TP2_Grupo19/src/main/java/fi/unju/edu/ar/Tp2Grupo19Application.java
@@ -0,0 +1,13 @@
+package fi.unju.edu.ar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2Grupo19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2Grupo19Application.class, args);
+	}
+
+}
