bf26de1815dc1d589439e93c8b8be9e180ab1262
ignam03 <ignafree97@gmail.com>
2022-04-16 17:13:06 -0300

    punto9 realizado

---
+++ TP2_Grupo19/src/main/java/fi/unju/edu/ar/puntoNueve/Principal.java
@@ -0,0 +1,17 @@
+package fi.unju.edu.ar.puntoNueve;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		for (int i = 320; i >= 160;) {
+			System.out.println(i);
+			i -= 20;
+		}
+		
+	}
+
+}
