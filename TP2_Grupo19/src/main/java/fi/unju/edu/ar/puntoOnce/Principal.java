b313df1183e8213985d238bbb5047a19a6a3ed41
ignam03 <ignafree97@gmail.com>
2022-04-16 17:15:11 -0300

    punto11 realizado

---
+++ TP2_Grupo19/src/main/java/fi/unju/edu/ar/puntoOnce/Principal.java
@@ -0,0 +1,19 @@
+package fi.unju.edu.ar.puntoOnce;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num = 320;
+
+		do {
+
+			System.out.println(num);
+			num -= 20;
+
+		} while (num >= 160);
+
+	}
+
+}
