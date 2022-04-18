66dd5776874150e922df0daac08c2a4a3d7414a7
ignam03 <ignafree97@gmail.com>
2022-04-17 22:51:14 -0300

    punto7 falta

---
+++ TP2_Grupo19/src/main/java/fi/unju/edu/ar/puntoSiete/Principal.java
@@ -0,0 +1,21 @@
+package fi.unju.edu.ar.puntoSiete;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+
+		for(int j = 40; j >= -1;) {
+			for(int i = 2; j > 0;) {
+				j = j -i ;
+				System.out.println(j + " " );
+			}
+			
+		}
+		
+		
+	}
+
+}
