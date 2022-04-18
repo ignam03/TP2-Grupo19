de35c14ece956a00f5928145ad3e2ca7b26d1fb1
ignam03 <ignafree97@gmail.com>
2022-04-17 21:01:38 -0300

    punto14 realizado

--- TP2_Grupo19/src/main/java/fi/unju/edu/ar/puntoTrece/Empleado.java
+++ TP2_Grupo19/src/main/java/fi/unju/edu/ar/puntoTrece/Empleado.java
@@ -1,6 +1,4 @@
 package fi.unju.edu.ar.puntoTrece;
-
-import java.sql.Date;
 import java.util.Calendar;
 
 public class Empleado {
@@ -12,14 +10,73 @@ public class Empleado {
 	private int sueldo;
 	private int horasTrabajadas;
 
+	//Constructor sin parametros o por defecto
 	public Empleado() {
-		super();
+		
+	}
+	
+	//Constructor con parametros nombre, legajo, horasTrabajadas
+	
+	public Empleado(String nombre, int legajo, int horasTrabajadas) {
+		this.nombre = nombre;
+		this.legajo = -999;
+		this.horasTrabajadas = horasTrabajadas;
+	}
+	
+	
+	//Constructor con parametros nombre, fechaIngreso, horasTrabajadas
+	
+	public Empleado(String nombre, Calendar fechaIngreso, int horasTrabajadas) {
+		this.nombre = nombre;
+		this.fechaIngreso = fechaIngreso;
+		this.horasTrabajadas = horasTrabajadas;
+	}
+	
+	//Constructor con parametros nombre, horasTrabajadas
+	
+	public Empleado(String nombre, int horasTrabajadas) {
+		this.nombre = nombre;
+		this.horasTrabajadas = horasTrabajadas;
+	}
+	
+	
+	public String getNombre() {
+		return nombre;
 	}
 
 
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Calendar getFechaIngreso() {
+		return fechaIngreso;
+	}
+
+	public void setFechaIngreso(Calendar fechaIngreso) {
+		this.fechaIngreso = fechaIngreso;
+	}
+
+	public int getLegajo() {
+		return legajo;
+	}
+
+	public void setLegajo(int legajo) {
+		this.legajo = legajo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 	public int getSueldo() {
 		return sueldo;
 	}
+	
 
 	public void setSueldo(int sueldo) {
 		this.sueldo = sueldo;
--- TP2_Grupo19/src/main/java/fi/unju/edu/ar/puntoTrece/Principal.java
+++ TP2_Grupo19/src/main/java/fi/unju/edu/ar/puntoTrece/Principal.java
@@ -7,11 +7,22 @@ public class Principal {
 
 		
 		Empleado empleado1 = new Empleado();
-		
 		empleado1.setHorasTrabajadas(161);
-		
-	
 		System.out.println("El sueldo que le corresponde es: " + empleado1.calcularSueldo());
+		
+		//Objeto con constructor por defecto
+		Empleado empleado2 = new Empleado();
+		//Objeto con constructor parametrizado1
+		Empleado empleado3 = new Empleado(null, 0, 0);
+		//Objeto con constructor parametrizado2
+		Empleado empleado4 = new Empleado(null, null, 0);
+		//Objeto con constructor parametrizado2
+		Empleado empleado5 = new Empleado(null, 0);
+		
+		System.out.println(empleado2);
+		System.out.println(empleado3.getNombre() + " " + empleado3.getLegajo() + " " + empleado3.getHorasTrabajadas());
+		System.out.println(empleado4.getNombre() + " " + empleado4.getFechaIngreso() + " " + empleado4.getHorasTrabajadas() );
+		System.out.println(empleado5.getNombre() + " " + empleado5.getHorasTrabajadas());
 	}
 
 }
